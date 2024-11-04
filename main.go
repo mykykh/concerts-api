8485416c0b34651cc20c7f04986ac0c73461066e
Mykyta Kholiavko <49101849+mykykh@users.noreply.github.com>
2024-11-04 18:23:21 +0200

    add connection to db

--- main.go
+++ main.go
@@ -2,15 +2,40 @@ package main
 
 import (
     "net/http"
+    "context"
+    "fmt"
+    "os"
+
+    "github.com/go-chi/chi/v5"
+    "github.com/jackc/pgx/v5/pgxpool"
 
     "github.com/mykykh/concerts-api/internal/api"
-    "github.com/go-chi/chi/v5"
 )
+
+func testDBConnect() {
+	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	defer dbpool.Close()
+
+	var greeting string
+	err = dbpool.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(greeting)
+}
 
 func main() {
     r := chi.NewRouter()
 
     r.Mount("/concerts", api.ConcertsResource{}.Routes())
+
+    testDBConnect()
 
     http.ListenAndServe(":8080", r)
 }
